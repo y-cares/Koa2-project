87de8797f832437ba79f43d5d65ace74312034d1
造船熊 <447936712@qq.com>
2018-08-02 21:33:26 +0800

    feat: 新增了html静态模板文件

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const Koa = require('koa')
+const { normal } = require('./tpl')
+
 const app = new Koa()
 
 app.use(async (ctx, next) => {
-    ctx.body = "电影首页"
+    ctx.type = 'text/html;charset=utf-8'
+    ctx.body = normal
 })
 .listen(3000, () => {
     console.log('Server is Start')
---
+++ server/tpl/normal.js
@@ -0,0 +1,27 @@
+module.exports = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Koa Server HTML</title>
+        <link href="https://cdn.bootcss.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.bootcss.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js"></script>
+        <script src="https://cdn.bootcss.com/jquery/3.3.0/jquery.min.js"></script>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <h1>Hi Luke</h1>
+                    <p>This is Scott</p>
+                </div>
+                <div class="col-md-4">
+                    <p>测试静态 html 页面</p>
+                </div>
+            </div>
+        </div>
+    </body>
+    </html>
+`
